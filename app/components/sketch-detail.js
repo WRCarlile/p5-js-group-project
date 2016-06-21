df9004c46ec5a2e98bff2c0aeca2c8add9f10d84
Josh Bertsche <jjjbertsche@gmail.com>
2016-06-20 23:17:28 -0700

    embed p5 sketch in component using 'getScript'

--- app/components/sketch-detail.js
+++ app/components/sketch-detail.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  loadPlugin: function () {
+    Ember.run.scheduleOnce('afterRender', this, function () {
+      Ember.$.getScript('/assets/josh-sketch.js').then(function () {
+        console.log("done");
+        Ember.$.getScript('//cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.2/p5.js');
+      });
+    })
+  }.on('init')
 
 });
---
+++ public/assets/josh-sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+console.log("josh's setup");
+}
+
+function draw() {
+console.log("josh's draw");
+  ellipse(50, 50, 80, 80);
+}
+
+console.log("hi");
